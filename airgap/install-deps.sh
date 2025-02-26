ebb5f1604dcac4d422ee89a9cdd524c1dbc59748
Debian <admin@airgap>
2025-02-26 20:14:35 +0000

    more

--- airgap/install-deps.sh
+++ airgap/install-deps.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 
 # Setup CA tool and directory
-sudo cp gen-ca.sh /opt/bin
+sudo mkdir -p /opt/bin
+sudo cp gen-ca.sh /opt/bin/
 sudo chmod +x /opt/bin/gen-ca.sh
 sudo mkdir -p /opt/certs
