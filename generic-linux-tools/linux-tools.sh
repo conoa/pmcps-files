d72207968c051d4acc3e8dc75372e2f328bdfbf2
Mårten Cassel <marten.cassel@gmail.com>
2025-02-26 20:59:03 +0100

    more

---
+++ generic-linux-tools/linux-tools.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get update
+
+apt-get -y install \
+    curl \
+    dnsutils \
+    e2fsprogs \
+    fdisk \
+    git \
+    htop \
+    iputils-ping \
+    logrotate \
+    lsof \
+    nano \
+    net-tools \
+    openssh-client \
+    parted \
+    rsync \
+    strace \
+    vim \
+    wget
---
+++ mgmt/install-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install gnupg vim net-tools
+sudo apt-get -y install ansible python3 python3-venv
+sudo apt-get -y install jq yq docker.io helm
+
+wget https://get.helm.sh/helm-v3.17.1-linux-amd64.tar.gz -O /tmp/helm.tar.gz
+tar xvf /tmp/helm.tar.gz -C /tmp/
+cp /tmp/linux-amd64/helm /tmp/helm
+
+wget https://github.com/kubernetes-sigs/krew/releases/download/v0.4.4/krew-linux_amd64.tar.gz -O /tmp/krew.tar.gz
+tar xvf /tmp/krew.tar.gz -C /tmp
+tar xvf /tmp/krew.tar.gz -C /tmp
+
+
