bde35a74ff68754ad6a415d2079d8b4b986d66af
Mårten Cassel <marten.cassel@gmail.com>
2025-02-26 22:11:34 +0100

    more

--- airgap/gen-server-cert.sh
+++ airgap/gen-server-cert.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /opt || exit
-sudo /opt/bin/gen-ca.sh generate-ca
 sudo /opt/bin/gen-ca.sh issue-cert ec2-13-50-126-32.eu-north-1.compute.amazonaws.com ec2-13-50-126-32.eu-north-1.compute.amazonaws.com
 openssl x509 -in /opt/certs/ec2-13-50-126-32.eu-north-1.compute.amazonaws.com.crt -text -noout
 ls -l /opt/certs
